2fffcdcc0c8503d8a61e44d3f5c9d31ad48fc8c8
canadasnowman <114317119+canadasnowman@users.noreply.github.com>
2024-12-23 15:32:24 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class Main {
     static int[][] A;
     static int[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int N,K;
+    static int[] dx = {0, -1, 1, 0};
+    static int[] dy = {-1, 0, 0, 1};
+    static int X,Y;
     static int count;
 
     public static void main(String[] args) throws IOException{
@@ -17,24 +17,28 @@ public class Main {
 
         for (int l = 0; l < repeat; l++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int colunms = Integer.parseInt(st.nextToken());
+            int columns = Integer.parseInt(st.nextToken());//x축과 y축의 길이 받기
             int rows = Integer.parseInt(st.nextToken());
-            int vegt = Integer.parseInt(st.nextToken());
-            A = new int[colunms][rows];
-            visited = new int[colunms][rows];
+            int M = Integer.parseInt(st.nextToken());
+
+            X = columns;
+            Y = rows;
+
+            A = new int[columns][rows];
+            visited = new int[columns][rows];
 
             count = 0;
-            N = colunms;
-            K = rows;
 
-            for (int i = 0; i < vegt; i++) {
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                A[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+                int tmpx = Integer.parseInt(st.nextToken());
+                int tmpy = Integer.parseInt(st.nextToken());
+                A[tmpx][tmpy] = 1;
             }
 
-            for (int i = 0; i < colunms; i++) {
-                for (int j = 0; j < rows; j++) {
-                    if(A[i][j] == 1) {
+            for (int i = 0; i < X; i++) {
+                for (int j = 0; j < Y; j++) {
+                    if (A[i][j] == 1 && visited[i][j] != 1) {
                         BFS(i, j);
                     }
                 }
@@ -44,29 +48,24 @@ public class Main {
 
     }
     public static void BFS(int x, int y){
-        if (visited[x][y] == 0) {
-            count++;
-        }
-
-        Queue<Integer> queueX = new LinkedList<>();
-        Queue<Integer> queueY = new LinkedList<>();
-
-        queueX.add(x);
-        queueY.add(y);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
         visited[x][y] = 1;
 
-        while(!queueX.isEmpty()&&!queueY.isEmpty()){
-            int xx = queueX.poll();
-            int yy = queueY.poll();
+        count++;
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int cx = current[0];
+            int cy = current[1];
 
             for (int i = 0; i < 4; i++) {
-                int bx = xx - dx[i];
-                int by = yy - dy[i];
-                if (bx >= 0 && by >= 0 && bx < N && by < K) {
-                    if (A[bx][by] == 1 && visited[bx][by] == 0) {
-                        queueX.add(xx - dx[i]);
-                        queueY.add(yy - dy[i]);
-                        visited[xx - dx[i]][yy - dy[i]] = 1;
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (nx >= 0 && nx < X && ny >= 0 && ny < Y) {
+                    if (A[nx][ny] == 1 && visited[nx][ny] != 1) {
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = 1;
                     }
                 }
             }
